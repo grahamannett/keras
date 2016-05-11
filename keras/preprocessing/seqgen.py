7fa231d346cccb651a821f5ae94e6a5dfec60839
graham <graham.annett@gmail.com>
2016-05-11 15:25:37 -0700

    added vocab section

--- keras/preprocessing/seqgen.py
+++ keras/preprocessing/seqgen.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 '''
     example:
     dataset being [1,2,3,..,98,99,100]
@@ -7,14 +8,27 @@
 
     x_step = 2
 
+=======
+class SequenceDataGen(object):
+        '''
+        https://gist.github.com/braingineer/c69482eb1bfa4ac3bf9a7bc9b6b35cdf
+        Generate minibatches on sequences with
+    data augmentation.
+
+    # Arguments
+        step_size: set input mean to 0 over the dataset.
+        y_length: number of elements to use in y
+
+    example:
+    step
+>>>>>>> Stashed changes
     X                y
     [1,2,3,4],       [5,6]
     [3,4,5,6],       [7,8]
     [5,6,7,8],       [9,10]
     ...
+<<<<<<< Updated upstream
     [95,96,97,98],[99,100]
-
-
 
 also:
 
@@ -30,16 +44,10 @@ also:
         t.flow(d)
 
     '''
-
+import numpy as np
 
 class SequenceDataGen:
     '''Generate batches on time series/sequential data
-
-    to add:
-    vocab from:
-        https://gist.github.com/braingineer/c69482eb1bfa4ac3bf9a7bc9b6b35cdf
-        https://github.com/braingineer/ikelos/blob/master/ikelos/data/data_server.py
-
     # Arguments
         step_size: set input mean to 0 over the dataset.
 
@@ -83,6 +91,21 @@ class SequenceDataGen:
 
         self.batch_size = batch_size
         self.shuffle = shuffle
+=======
+    [99,100,101,102],[103,104]
+    '''
+
+    def __init__(self,
+                 step_size=1,
+                 y_length=1):
+        self.x = x
+        self.y = y
+        self.x_step = 1
+        self.x_size = x.shape
+        self.y_step = 1
+        self.y_size = y.shape
+        self.x_y_size = 1
+>>>>>>> Stashed changes
 
     def reset(self):
         self.batch_index = 0
@@ -90,12 +113,17 @@ class SequenceDataGen:
     def move_window(self):
         pass
 
+<<<<<<< Updated upstream
     def gen_possible_indices(self, X):
         '''
         get possible indices's for time series given
         '''
+        if isinstance(X, str):
+            X_shape = len(X)
+        else:
+            X_shape = X.shape[0]
         last_window = self.x_window + self.y_window + self.x_y_diff
-        idxes = list(range(0, X.shape[0] - last_window, self.x_step))
+        idxes = list(range(0, X_shape - last_window, self.x_step))
         if self.shuffle:
             from random import shuffle
             shuffle(idxes)
@@ -118,7 +146,8 @@ class SequenceDataGen:
         while idxes_:
             batch_ = idxes_[:self.batch_size]
             del idxes_[:self.batch_size]
-            yield [self.generate_x_y(data, b) for b in batch_]
+            gend = [self.generate_x_y(data, b) for b in batch_]
+            yield gend
 
     def flow(self, data, seed=None):
         self.idxes = self.gen_possible_indices(data)
@@ -126,10 +155,117 @@ class SequenceDataGen:
         self.flow_generator = self._flow_batch(data)
         return self
 
-    def from_vocab(self):
-        pass
+    def flow_from_vocab(self, data, words=False):
+        '''vocab from:
+        https://gist.github.com/braingineer/c69482eb1bfa4ac3bf9a7bc9b6b35cdf
+        github.com/braingineer/ikelos/blob/master/ikelos/data/data_server.py
+
+        other notes:
+        github.com/fchollet/keras/blob/master/examples/lstm_text_generation.
+
+        '''
+        # split by words or chars
+        if words:
+            # # implement later
+            # things_to_replace = {'\n': ' ', '--': ' -- '}
+            # for k, v in things_to_replace.items():
+            #     data = data.replace(k, v)
+            # self.tokens = set(self.data.split(' '))
+            pass
+        else:
+            self.tokens = set(data)
+
+        self.idxes = self.gen_possible_indices(data)
+
+        self.token_indices = dict((c, i) for i, c in enumerate(self.tokens))
+        self.indices_token = dict((i, c) for i, c in enumerate(self.tokens))
+        # vectorization
+
+        X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+        y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+        for i, sentence in enumerate(sentences):
+            for t, char in enumerate(sentence):
+                X[i, t, char_indices[char]] = 1
+            y[i, char_indices[next_chars[i]]] = 1
 
     def __iter__(self):
+=======
+    def _flow_index(self, N, batch_size=32, shuffle=False, seed=None):
+        while 1:
+            index_array = np.arange(N)
+            if self.batch_index == 0:
+                if shuffle:
+                    if seed is not None:
+                        np.random.seed(seed + self.total_batches_seen)
+                    index_array = np.random.permutation(N)
+
+            current_index = (self.batch_index * batch_size) % N
+            if N >= current_index + batch_size:
+                current_batch_size = batch_size
+                self.batch_index += 1
+            else:
+                current_batch_size = N - current_index
+                self.batch_index = 0
+            self.total_batches_seen += 1
+            yield (index_array[current_index: current_index + current_batch_size],
+                   current_index, current_batch_size)
+
+    def flow(self, X, y, batch_size=32, shuffle=False, seed=None,
+             save_to_dir=None, save_prefix='', save_format='jpeg'):
+        assert len(X) == len(y)
+        self.X = X
+        self.y = y
+        self.save_to_dir = save_to_dir
+        self.save_prefix = save_prefix
+        self.save_format = save_format
+        self.reset()
+        self.flow_generator = self._flow_index(X.shape[0], batch_size,
+                                               shuffle, seed)
+        return self
+
+            def __iter__(self):
+>>>>>>> Stashed changes
         # needed if we want to do something like:
         # for x, y in data_gen.flow(...):
         return self
+
+<<<<<<< Updated upstream
+'''       EXAMPLE :
+        ['preface\n\n\nsupposing that truth is a woma',
+        'face\n\n\nsupposing that truth is a woman--',
+        'e\n\n\nsupposing that truth is a woman--wha']
+        self.x_window = 40
+        x_window=10,
+        y_window=1,
+        x_step=3,
+        x_y_diff=0,'''
+=======
+    def next(self):
+        # for python 2.x.
+        # Keeps under lock only the mechanism which advances
+        # the indexing of each batch
+        # see # http://anandology.com/blog/using-iterators-and-generators/
+        with self.lock:
+            index_array, current_index, current_batch_size = next(self.flow_generator)
+        # The transformation of images is not under thread lock so it can be done in parallel
+        bX = np.zeros(tuple([current_batch_size] + list(self.X.shape)[1:]))
+        for i, j in enumerate(index_array):
+            x = self.X[j]
+            x = self.random_transform(x.astype('float32'))
+            x = self.standardize(x)
+            bX[i] = x
+        if self.save_to_dir:
+            for i in range(current_batch_size):
+                img = array_to_img(bX[i], self.dim_ordering, scale=True)
+                fname = '{prefix}_{index}.{format}'.format(prefix=self.save_prefix,
+                                                           index=current_index + i,
+                                                           format=self.save_format)
+                img.save(os.path.join(self.save_to_dir, fname))
+        bY = self.y[index_array]
+        return bX, bY
+
+    def __next__(self):
+        # for python 3.x.
+        return self.next()
+
+>>>>>>> Stashed changes
